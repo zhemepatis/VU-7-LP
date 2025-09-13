a8af4564b5dadcf6c3ff772adf34826bfbf49ed6
zhemepatis <rink.gab@gmail.com>
2025-09-13 20:11:29 +0300

    removed unused files; added ppl age and their hobbies

--- lab-01/genealogy_tree.pl
+++ lab-01/genealogy_tree.pl
@@ -3,46 +3,39 @@
 % trys_draugai(Draugas1, Draugas2, Draugas3) - Asmenys Draugas1, Draugas2 ir Draugas3 yra panašaus amžiaus ir turi tą patį pomėgį;
 % gera_pora(Asmuo1, Asmuo2) - Asmenys Asmuo1 ir Asmuo2 yra panašaus amžiaus ir turi tą patį pomėgį;
 
-% asmuo(Vardas, Lytis, Amžius, Pomėgis);
-% mama(Mama, Vaikas);
-% pora(Vyras, Žmona).
-
 % pirma seima
-asmuo(lauma, moteris, ??, ??).
-asmuo(deivids, vyras, ??, ??).
+asmuo(lauma, moteris, 34, begimas).
+asmuo(deivids, vyras, 35, begimas).
+
+asmuo(aivars, vyras, 10, futbolas).
+asmuo(aina, moteris, 12, gimnastika).
+asmuo(arina, moteris, 13, muzika).
+asmuo(anete, moteris, 14, muzika).
 
 pora(deivids, lauma).
-
-asmuo(aivars, vyras, ??, ??).
-asmuo(aina, moteris, ??, ??).
-asmuo(arina, moteris, ??, ??).
-asmuo(anete, moteris, ??, ??).
-    
 mama(lauma, aivars).
 mama(lauma, aina).
 mama(lauma, arina).
 mama(lauma, anete).
     
 % antra seima
-asmuo(dana, moteris, ??, ??).
-asmuo(jans, vyras, ??, ??).
+asmuo(dana, moteris, 30, mezgimas).
+asmuo(jans, vyras, 31, keliavimas).
+
+asmuo(vita, moteris, 4, puzles).
+asmuo(ulrika, moteris, 9, futbolas).
 
 pora(jans, dana).
-
-asmuo(vita, moteris, ??, ??).
-asmuo(ulrika, moteris, ??, ??).
-
 mama(dana, ulrika).
 mama(dana, vita).
 
 % trecia seima
-asmuo(upe, moteris, ??, ??).
-asmuo(klints, vyras, ??, ??).
+asmuo(upe, moteris, 35, keliavimas).
+asmuo(klints, vyras, 43, keliavimas).
+
+asmuo(kristaps, vyras, 10, futbolas).
+asmuo(laime, moteris, 14, muzika).
 
 pora(klints, upe).
-
-asmuo(kristaps, vyras, ??, ??).
-asmuo(laime, moteris, ??, ??).
-
 mama(upe, laime).
 mama(upe, kristaps).
